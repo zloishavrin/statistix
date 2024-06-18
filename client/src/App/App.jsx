35b2a2f4648357444feaaf6483215ff4e3d98821
zloishvarin <nexxxusoffical@gmail.com>
2024-06-18 14:16:02 +0500

    add transition-switch

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,18 +1,27 @@
 import './reset.css';
 import './global.css';
 import './scrollbar.css';
+import './transition.css';
 import styles from './App.module.css';
 import Main from "../Pages/Main/Main";
 import Build from "../Pages/Build/Build";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 function App() {
+
+  const location = useLocation();
+
   return (
     <div className={styles.App}>
-      <Routes>
-        <Route path="/" element={<Main/>}/>
-        <Route path="/build/*" element={<Build/>}/>
-      </Routes>
+      <SwitchTransition>
+          <CSSTransition key={location.key} classNames="transition-fade" timeout={500}>
+            <Routes location={location}>
+              <Route path="/" element={<Main/>}/>
+              <Route path="/build/*" element={<Build/>}/>
+            </Routes>
+          </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 }
