653b9a67e1784f972fb5401d604dfea9e82e9e5d
zloishvarin <nexxxusoffical@gmail.com>
2024-06-18 20:52:32 +0500

    add table

---
+++ client/src/Components/Table/Table.jsx
@@ -0,0 +1,52 @@
+import styles from "./Table.module.css";
+
+const Table = ({ data, labels }) => {
+    
+    console.log(data);
+
+    const labelsRow = [];
+    for(let index = 0; index < 11; index ++) {
+        if(labels[index]) {
+            labelsRow.push(<th>{labels[index]}</th>);
+        }
+        else {
+            labelsRow.push(<th> </th>);
+        }
+    }
+
+    const tableRows = [];
+    for(let index = 0; index < 101; index ++)  {
+        const tableRow = [];
+        tableRow.push(<td>{index}</td>);
+        for(let jIndex = 0; jIndex < 11; jIndex ++) {
+            if(data[jIndex] && data[jIndex][index]) {
+                tableRow.push(<td>{data[jIndex][index]}</td>);
+            }
+            else if(index === 100 && data[jIndex]) {
+                tableRow.push(<td>...</td>);
+            }
+            else {
+                tableRow.push(<td> </td>);
+            }
+        }
+        tableRows.push(<tr>{tableRow}</tr>);
+    }
+
+    return (
+        <div className={styles.TableContainer}>
+            <table className={styles.Table}>
+                <thead>
+                    <tr>
+                        <th> </th>
+                        {labelsRow}
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableRows}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Table;
--- client/src/Features/Arima/Arima.jsx
+++ client/src/Features/Arima/Arima.jsx
@@ -1,11 +1,38 @@
 import Input from "../../Components/Input/Input";
+import Table from "../../Components/Table/Table";
 import { useEffect, useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import * as XLSX from 'xlsx';
 
 const Arima = () => {
 
     const [ pValue, setPValue ] = useState(0);
     const [ dValue, setDValue ] = useState(0);
-    const [ QValue, setQValue ]  = useState(0);
+    const [ QValue, setQValue ] = useState(0);
+    const [ next, setNext ]  = useState(0);
+
+    const [ file, setFile ] = useState(null);
+    const [ data, setData ] = useState(null);
+
+    useEffect(() => {
+        if(file) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const workbook = XLSX.read(event.target.result, { type: 'binary' });
+                const sheetName = workbook.SheetNames[0];
+                const sheet = workbook.Sheets[sheetName];
+                const sheetData = XLSX.utils.sheet_to_json(sheet);
+                const dataArrayOfObject = sheetData.slice(0, 100);
+                const dataArrayOfNumber = dataArrayOfObject.map(obj => Object.values(obj)[0]);
+                setData(dataArrayOfNumber);
+            };
+            reader.readAsBinaryString(file);
+        }
+    }, [file]);
+
+    useEffect(() => {
+        console.log([data]);
+    }, [data]);
 
     return (
         <>
@@ -15,20 +42,47 @@ const Arima = () => {
             </div>
             <div className="FeaturesInputsContainer">
                 <div className="FeaturesAreaContainer">
-                    <Input 
+                    <Input
                         TaleTitle="P"
                         setter={(e) => setPValue(e)}
                     />
-                    <Input 
+                    <Input
                         TaleTitle="D"
                         setter={(e) => setDValue(e)}
                     />
-                    <Input 
+                    <Input
                         setter={(e) => setQValue(e)}
                         TaleTitle="Q"
                     />
+                    <Input
+                        setter={(e) => setNext(e)}
+                        TaleTitle="Прогноз"
+                    />
+                    <button
+                        className="FeaturesButton"
+                    >
+                        Запустить
+                    </button>
                 </div>
                 <div className="FeaturesTableContainer">
+                    {
+                        data ?
+                        <Table 
+                            data={[data]}
+                            labels={['y(t)']}
+                        />
+                        :
+                        <FileUploader
+                            handleChange={(file) => setFile(file)}
+                            name="file"
+                            classes="FeaturesFileUploader"
+                            types={['xls', 'xlsx']}
+                        >
+                            <h1>Загрузка файла</h1>
+                            <p>Поддерживаются xls, xlsx и csv файлы</p>
+                        </FileUploader>
+                    }
+                    
                 </div>
             </div>
         </>
