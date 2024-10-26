a38a4de067e4de10d525d9786dcc1eae3e125146
zloishvarin <nexxxusoffical@gmail.com>
2024-10-26 22:38:15 +0300

    frontend: add footer

--- client/src/Pages/Main/Main.jsx
+++ client/src/Pages/Main/Main.jsx
@@ -144,6 +144,12 @@ const Main = () => {
                     </div>
                 </div>
             </div>
+            <footer className={styles.Footer}>
+                <a href="https://statistix.shaligula.ru/conf">Конфиденциальность данных</a>
+                <a href="https://api.statistix.shaligula.ru/docs">API</a>
+                <a href="https://shaligula.ru">Шалигула</a>
+                <a href="https://github.com/zloishavrin/statistix">GitHub</a>
+            </footer>
         </>
     )
 
