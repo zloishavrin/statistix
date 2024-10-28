3d6b008408e4bd7df495038e0baaa34364ddadde
zloishvarin <nexxxusoffical@gmail.com>
2024-10-28 14:41:31 +0300

    frontend: add chart download

--- client/src/Components/Chart/Chart.jsx
+++ client/src/Components/Chart/Chart.jsx
@@ -9,7 +9,9 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
+import { useRef } from "react";
 import { Line } from 'react-chartjs-2';
+import html2canvas from "html2canvas";
 
 ChartJS.register(
     CategoryScale,
@@ -22,6 +24,9 @@ ChartJS.register(
 );
 
 const Chart = ({ dataset, title, label }) => {
+
+    const chartRef = useRef(null);
+
     const options = {
         maintainAspectRatio: false,
         responsive: true,
@@ -62,10 +67,40 @@ const Chart = ({ dataset, title, label }) => {
         }))
     };
 
+    const saveAsImage = () => {
+        if (chartRef.current) {
+          html2canvas(chartRef.current).then(canvas => {
+            const imgData = canvas.toDataURL('image/png');
+            const link = document.createElement('a');
+            link.href = imgData;
+            link.download = `График.png`;
+            link.click();
+          });
+        }
+      };
+
     return (
         <div className={styles.Container}>
             <div className={styles.ChartContainer}>
-                <div className={styles.Chart}>
+                <button
+                    className={styles.Download}
+                    onClick={saveAsImage}
+                >
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0">
+                        </g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
+                        </g>
+                        <g id="SVGRepo_iconCarrier"> 
+                            <path d="M8 12L12 16M12 16L16 12M12 16V8M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            </path> 
+                        </g>
+                    </svg>
+                </button>
+                <div 
+                    className={styles.Chart}
+                    ref={chartRef}
+                >
                     <Line
                         options={options}
                         data={data}
