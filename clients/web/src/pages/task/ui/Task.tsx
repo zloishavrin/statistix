d3320c1c34df94e6be1727a7d466db4a195d45c9
Nikita Shavrin <teabrosers@gmail.com>
2026-06-19 12:08:52 +0300

    fix cascade-layers on input in task component

--- clients/web/src/pages/task/ui/Task.tsx
+++ clients/web/src/pages/task/ui/Task.tsx
@@ -7,11 +7,14 @@ import { TablePreview } from "@entities/file";
 import { useTranslation } from "react-i18next";
 import { Input } from "@shared/ui/input";
 import { DataPreview } from "@widgets/data-preview";
+import { useState } from "react";
 
 export const Task = () => {
 	const { task, inputTableVisibleOptions, resultTableVisibleOptions } =
 		useTask();
 	const { t } = useTranslation("task");
+
+	const [hoveredId, setHoveredId] = useState<string | null>(null);
 
 	return (
 		<div className={styles.Container}>
@@ -37,11 +40,16 @@ export const Task = () => {
 										key={param.id}
 										className={styles.InputParamItem}
 										labelWrapperStyles={{
-											zIndex: array.length - index,
+											zIndex:
+												hoveredId === param.id ? 9999 : array.length - index,
 										}}
 										label={
-											<div className={styles.LabelContainer}>
-												<p>{param.title}</p>
+											<div
+												className={styles.LabelContainer}
+												onMouseEnter={() => setHoveredId(param.id)}
+												onMouseLeave={() => setHoveredId(null)}
+											>
+												<p className={styles.LabelTitle}>{param.title}</p>
 												<div className={styles.QuestLabel}>
 													<p className={styles.QuestLabelText}>?</p>
 													<div className={styles.HelpContainer}>
@@ -80,17 +88,28 @@ export const Task = () => {
 							task.result.params &&
 							task.result.params.length > 0 && (
 								<div className={styles.OutputParamGrid}>
-									{task.result.params.map((param) => (
+									{task.result.params.map((param, index, array) => (
 										<Input
 											value={param.value.toString()}
 											disabled
 											key={param.id}
+											labelWrapperStyles={{
+												zIndex:
+													hoveredId === param.id ? 9999 : array.length - index,
+											}}
 											label={
-												<div className={styles.LabelContainer}>
-													<p>{param.title}</p>
+												<div
+													className={styles.LabelContainer}
+													onMouseEnter={() => setHoveredId(param.id)}
+													onMouseLeave={() => setHoveredId(null)}
+												>
+													<p className={styles.LabelTitle}>{param.title}</p>
 													<div className={styles.QuestLabel}>
 														<p className={styles.QuestLabelText}>?</p>
 														<div className={styles.HelpContainer}>
+															<p className={styles.LabelExtensionTitle}>
+																{param.title}
+															</p>
 															<p>{param.description}</p>
 														</div>
 													</div>
